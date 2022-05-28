4685f543b560516e701c8fd1f1332eeb693fbfd8
PavelTitov16 <pashoktitov16@gmail.com>
2022-05-28 16:15:33 +0300

    refactor: rewrite double quotes into single quotes in JS according to rule 1.3 in html-and-css.md

--- app.js
+++ app.js
@@ -8,45 +8,45 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-const taskInput = document.getElementById("todo-app__new-task");//Add a new task.
-const addButton = document.querySelector(".todo-app__task-btn");//first button
-const incompleteTaskHolder = document.querySelector(".todo-app__tasks-undone-list");//ul of #incompleteTasks
-const completedTasksHolder = document.querySelector(".todo-app__tasks-done-list");//completed-tasks
+const taskInput = document.getElementById('todo-app__new-task');//Add a new task.
+const addButton = document.querySelector('.todo-app__task-btn');//first button
+const incompleteTaskHolder = document.querySelector('.todo-app__tasks-undone-list');//ul of #incompleteTasks
+const completedTasksHolder = document.querySelector('.todo-app__tasks-done-list');//completed-tasks
 
 
 //New task list item
 const createNewTaskElement = function(taskString) {
 
-  const listItem = document.createElement("li");
-  listItem.className =  "todo-app__tasks-list-item";
+  const listItem = document.createElement('li');
+  listItem.className =  'todo-app__tasks-list-item';
   //input (checkbox)
-  const checkBox = document.createElement("input");//checkbx
+  const checkBox = document.createElement('input');//checkbx
   //label
-  const label = document.createElement("label");//label
+  const label = document.createElement('label');//label
   //input (text)
-  const editInput = document.createElement("input");//text
+  const editInput = document.createElement('input');//text
   //button.edit
-  const editButton = document.createElement("button");//edit button
+  const editButton = document.createElement('button');//edit button
 
   //button.delete
-  const deleteButton = document.createElement("button");//delete button
-  const deleteButtonImg = document.createElement("img");//delete button image
+  const deleteButton = document.createElement('button');//delete button
+  const deleteButtonImg = document.createElement('img');//delete button image
 
   label.innerText = taskString;
   label.className = 'todo-app__task-label';
-  label.setAttribute("for", "todo-app__task");
+  label.setAttribute('for', 'todo-app__task');
   //Each elements, needs appending
-  checkBox.type = "checkbox";
-  checkBox.className = "todo-app__checkbox";
-  editInput.type = "text";
-  editInput.className = "todo-app__task-input";
+  checkBox.type = 'checkbox';
+  checkBox.className = 'todo-app__checkbox';
+  editInput.type = 'text';
+  editInput.className = 'todo-app__task-input';
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "todo-app__edit-btn todo-app__btn";
+  editButton.className = 'todo-app__edit-btn todo-app__btn';
 
-  deleteButton.className = "todo-app__delete-btn todo-app__btn";
+  deleteButton.className = 'todo-app__delete-btn todo-app__btn';
   deleteButtonImg.src = './remove.svg';
-  deleteButtonImg.className = "todo-app__delete-img";
+  deleteButtonImg.className = 'todo-app__delete-img';
   deleteButton.appendChild(deleteButtonImg);
 
 
@@ -62,7 +62,7 @@ const createNewTaskElement = function(taskString) {
 
 
 const addTask = function() {
-  console.log("Add Task...");
+  console.log('Add Task...');
   //Create a new list item with the text from the #new-task:
   if (!taskInput.value) return;
   const listItem = createNewTaskElement(taskInput.value);
@@ -71,43 +71,43 @@ const addTask = function() {
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
 
-  taskInput.value = "";
+  taskInput.value = '';
 
 }
 
 //Edit an existing task.
 
 const editTask = function() {
-  console.log("Edit Task...");
-  console.log("Change 'edit' to 'save'");
+  console.log('Edit Task...');
+  console.log(`Change 'edit' to 'save'`);
 
 
   const listItem = this.parentNode;
 
   const editInput = listItem.querySelector('.todo-app__task-input');
-  const label = listItem.querySelector(".todo-app__task-label");
-  const editBtn = listItem.querySelector(".todo-app__edit-btn");
-  const containsClass = listItem.classList.contains("todo-app__tasks-list-item__redactored");
+  const label = listItem.querySelector('.todo-app__task-label');
+  const editBtn = listItem.querySelector('.todo-app__edit-btn');
+  const containsClass = listItem.classList.contains('todo-app__tasks-list-item__redactored');
   //If class of the parent is .editmode
   if (containsClass) {
 
     //switch to .editmode
     //label becomes the inputs value.
     label.innerText = editInput.value;
-    editBtn.innerText = "Edit";
+    editBtn.innerText = 'Edit';
   } else {
     editInput.value = label.innerText;
-    editBtn.innerText = "Save";
+    editBtn.innerText = 'Save';
   }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle("todo-app__tasks-list-item__redactored");
+  listItem.classList.toggle('todo-app__tasks-list-item__redactored');
 };
 
 
 //Delete task.
 const deleteTask = function() {
-  console.log("Delete Task...");
+  console.log('Delete Task...');
 
   const listItem = this.parentNode;
   const ul = listItem.parentNode;
@@ -119,7 +119,7 @@ const deleteTask = function() {
 
 //Mark task completed
 const taskCompleted = function() {
-  console.log("Complete Task...");
+  console.log('Complete Task...');
 
   //Append the task list item to the #completed-tasks
   const listItem = this.parentNode;
@@ -130,7 +130,7 @@ const taskCompleted = function() {
 
 
 const taskIncomplete = function() {
-  console.log("Incomplete Task...");
+  console.log('Incomplete Task...');
   //Mark task as incomplete.
   //When the checkbox is unchecked
   //Append the task list item to the #incompleteTasks.
@@ -142,7 +142,7 @@ const taskIncomplete = function() {
 
 
 const ajaxRequest = function() {
-  console.log("AJAX Request");
+  console.log('AJAX Request');
 }
 
 //The glue to hold it all together.
@@ -150,16 +150,16 @@ const ajaxRequest = function() {
 
 //Set the click handler to the addTask function.
 addButton.onclick = addTask;
-addButton.addEventListener("click", addTask);
-addButton.addEventListener("click", ajaxRequest);
+addButton.addEventListener('click', addTask);
+addButton.addEventListener('click', ajaxRequest);
 
 
 const bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
-  console.log("bind list item events");
+  console.log('bind list item events');
   //select ListItems children
-  const checkBox = taskListItem.querySelector(".todo-app__checkbox");
-  const editButton = taskListItem.querySelector(".todo-app__edit-btn");
-  const deleteButton = taskListItem.querySelector(".todo-app__delete-btn");
+  const checkBox = taskListItem.querySelector('.todo-app__checkbox');
+  const editButton = taskListItem.querySelector('.todo-app__edit-btn');
+  const deleteButton = taskListItem.querySelector('.todo-app__delete-btn');
 
 
   //Bind editTask to edit button.
